57be9ac17df50963ed8e86fcd0c5f784af72eebc
Rémi Chaignon <remi.chaignon@gmail.com>
2015-03-25 15:58:41 -0600

    Create organization

--- app/router.js
+++ app/router.js
@@ -1,13 +1,19 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function () {
-  this.route('organizations');
-  this.resource('organization', { path: '/organization/:organization_id' });
+  this.resource("organizations", function () {
+    this.route("create");
+  });
+  this.resource("organization", { path: "/organization/:organization_id" }, function () {
+    this.route("create");
+    this.route("edit");
+    this.route("delete");
+  });
 });
 
 export default Router;
